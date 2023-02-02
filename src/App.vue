7515c76a820f80ef9f0e9119200098dec58d3c4a
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-02-02 09:15:53 +0100

    REMAKE DU PROJET

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
-  <NavBar/>
-    
-  <img class="object-cover h-96 w-full" src="../src/assets/images/plante2.jpg">
-
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar
-  }
-}
-</script>
 
 <style>
 #app {
@@ -24,5 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
