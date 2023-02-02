8ce0676ee3a724ba09a847bd5439f5d12f8a8ae2
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-02-02 09:22:09 +0100

    ajout tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <NavBar></NavBar>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
