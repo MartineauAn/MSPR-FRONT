b6d7637defb9fe96689cdb5f584af3c7dea29c05
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-02-05 21:01:14 +0100

    MAinpage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="grid grid-cols-1 shadow-lg h-1/3">
+  <div class="relative">
+        <img src="@/assets/images/plante1.jpg" />
+        <h1 class="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            APlanter</h1>
+    </div>
+</div>
+<div class="grid grid-cols-3 text-center align-center">
+<div class="flex col-span-2 items-center justify-center mr-10 ml-10 h-full">
+  <h3>About us</h3>
+  <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum, diam ac finibus mattis, diam purus pellentesque mi, eget mattis velit neque ut quam. Nunc et velit .</p>
+</div>
+  <img class="object-cover h-3/4 w-full shadow-lg" src="@/assets/images/plante2.jpg">
+</div>
+<div class="grid grid-cols-1">
+  <P>BODY</P>
+  <P>BODY</P>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
