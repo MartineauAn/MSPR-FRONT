af2f3451d0cdbfd61fff53f537a3292a14b1388c
Cyprien Rimbaud <76480360+Sidarzo@users.noreply.github.com>
2023-02-06 20:36:02 +0100

    Home view front

---
+++ src/components/CardCatalogue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-white p-6 rounded-lg shadow-lg">
+    <img
+      src="@/assets/images/plante3.jpg"
+      alt="Example image 1"
+      class="w-full h-48 object-cover rounded-lg"
+    />
+    <div class="pt-6">
+      <p class="text-gray-600 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in
+        suscipit ante.
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,52 @@
 <template>
-<div class="grid grid-cols-1 shadow-lg h-1/3">
-  <div class="relative">
-        <img src="@/assets/images/plante1.jpg" />
-        <h1 class="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            APlanter</h1>
+<div style="background : linear-gradient(to right, rgb(226, 227, 221), rgb(212,219,212))">
+  <div class="bg-cover bg-center h-96 shadow-lg">
+    <h1
+      class="
+        absolute
+        text-5xl text-black
+        top-1/3
+        left-1/2
+        -translate-x-1/2 -translate-y-1/2
+      "
+    >
+      APlanter
+    </h1>
+    <img src="@/assets/images/plante1.jpg" class="object-cover h-full w-full" />
+  </div>
+  <div class="container mx-auto shadow-lg" style="background-color: #F6F6F8;">
+    <div class="grid grid-cols-2">
+      <div class="flex text-2xl justify-center content-center">About us</div>
+      <div>
+        <img
+          src="@/assets/images/plante2.jpg"
+          alt="Example image 1"
+          class="w-full h-96 object-cover"
+        />
+      </div>
     </div>
+  </div>
+
+  <div class="container mx-auto py-2 px-6 shadow-lg">
+    <h2 class="text-2xl font-bold mb-4">Catalogue de plantes</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:px-44 lg:gap-x-24 lg:gap-y-10 gap-6">
+      <CardCatalogue/>
+      <CardCatalogue/>
+      <CardCatalogue/>
+      <CardCatalogue/>
+      <CardCatalogue/>
+    </div>
+  </div>
+
 </div>
-<div class="grid grid-cols-3 text-center align-center">
-<div class="flex col-span-2 items-center justify-center mr-10 ml-10 h-full">
-  <h3>About us</h3>
-  <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum, diam ac finibus mattis, diam purus pellentesque mi, eget mattis velit neque ut quam. Nunc et velit .</p>
-</div>
-  <img class="object-cover h-3/4 w-full shadow-lg" src="@/assets/images/plante2.jpg">
-</div>
-<div class="grid grid-cols-1">
-  <P>BODY</P>
-  <P>BODY</P>
-</div>
-</template>
+ </template>
 
 <script>
+import CardCatalogue from '@/components/CardCatalogue.vue'
 
+export default {
+  components: {
+    CardCatalogue
+  }
+}
 </script>
